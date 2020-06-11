45a969844c43535f8445754f70f8e290616bc503
Gabriel Cesario <cesario203@outlook.com>
2020-06-11 20:15:16 -0300

    Estilizando body e entendendo como funciona o vue

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+ <div id="app">
+     <h1>Ola mundo/VUe</h1>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face{
+    font-family: 'RobotoMono';
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+*{
+    font-family: 'RobotoMono',monospace;
+}
+body{
+    margin: 0;
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: white;
+
+    background: linear-gradient(to right, rgb(223, 219, 8),rgb(156, 148, 31));
+}
+
+</style>
